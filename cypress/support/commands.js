0da069252edf602383248ac436c2050bb5960c37
Sam McTaggart <sam.mctaggart@gmail.com>
2020-01-29 18:44:08 +0000

    Setup cypress for testing
    As much as it would be great to be able to use Jest for testing, given that
    jest uses jsdom as a browser we'd be unable to test the text editor without
    stubbing out most of the functionality (due to jsdom not implimenting a lot
    of the native browser apis relied on by prosemirror).
    
    Instead we are using cypress, which offers us integration tests with a
    similar syntax to capyabra running in a real browser. Because of this, in a
    future commit we will need to setup a dummy app running the editor so that we
    can visit it in the browser.

---
+++ cypress/support/commands.js
@@ -0,0 +1,41 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+import 'cypress-file-upload';
+
+Cypress.Commands.add(
+  'keyboardShortcut',
+  {
+    prevSubject: true,
+  },
+  (subject, shortcut) => {
+    if (Cypress.platform === 'darwin') {
+      return cy.get(subject).type(`{meta}${shortcut}`);
+    } else {
+      return cy.get(subject).type(`{ctrl}${shortcut}`);
+    }
+  }
+)
