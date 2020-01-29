4e42d8d4ada94a4aad01c9d418d59971a2441014
Sam McTaggart <sam.mctaggart@gmail.com>
2020-01-29 18:53:33 +0000

    Setup basic prosemirror editor
    This commit introduces a function which we can use to instanciate the
    prosemirror editor.
    
    It's probably as vanilla a setup as we could hope for with basic key handling
    and the use of the schema introduced in the previous commit.
    
    The one special part here is `dispatchTransaction` which we use to fire a
    callback everytime the value of the editor changes. We will use this to keep
    our React component in sync with the editor.

---
+++ src/Editor/View.ts
@@ -0,0 +1,55 @@
+import { CreateEditorViewOptionsInterface } from './CreateEditorViewOptionsInterface';
+import { EditorView } from 'prosemirror-view';
+import { EditorState, Transaction } from 'prosemirror-state';
+import {
+  defaultMarkdownParser,
+  defaultMarkdownSerializer,
+  setupSchema,
+} from './markdown';
+import { keymap } from 'prosemirror-keymap';
+import { baseKeymap, exitCode, setBlockType } from 'prosemirror-commands';
+import { history, undo, redo } from 'prosemirror-history';
+import {
+  editorPlugin,
+} from './Plugins';
+
+const createEditorView = ({
+  node,
+  value,
+  onChange,
+  classes,
+  placeholder,
+  onToolbarChange,
+  imageUploadEndpoint,
+  disabledMarks,
+  disabledNodes,
+  onError,
+}: CreateEditorViewOptionsInterface): EditorView => {
+  const schema = setupSchema({ disabledMarks, disabledNodes });
+  const plugins = [
+    history(),
+    keymap({
+      'Mod-z': undo,
+      'Mod-y': redo,
+    }),
+    keymap(baseKeymap),
+    editorPlugin(classes, placeholder),
+  ];
+
+  const editorView = new EditorView(node, {
+    state: EditorState.create({
+      schema,
+      doc: defaultMarkdownParser(schema).parse(value),
+      plugins,
+    }),
+    dispatchTransaction(transation: Transaction) {
+      const newState = editorView.state.apply(transation);
+      editorView.updateState(newState);
+      onChange(defaultMarkdownSerializer.serialize(editorView.state.doc));
+    },
+  });
+
+  return editorView;
+};
+
+export { createEditorView };
