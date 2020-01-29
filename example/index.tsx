e0520079df917fe26c77685ab8811c23059c2b1c
Sam McTaggart <sam.mctaggart@gmail.com>
2020-01-29 18:46:39 +0000

    Setup example app
    We need this to be able to run our cypress tests against something. This
    example app uses the default setup provided by tsdx (so is using parcel to
    handle everything for us).
    
    As we get further in to the package development we'll add more and more to
    the dummy app.

---
+++ example/index.tsx
@@ -0,0 +1,14 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import MarkdownEditor from '../.';
+import '../dist/markdown-editor.esm.css';
+
+const App = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
