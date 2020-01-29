0dcf6087e9f6ac2b25ded63785a70b244519d6c1
Sam McTaggart <sam.mctaggart@gmail.com>
2020-01-29 13:51:46 +0000

    lintin

--- src/MarkdownEditorInterface.ts
+++ src/MarkdownEditorInterface.ts
@@ -1,7 +1,13 @@
 export type OnChangeFn = (value: string) => any;
 
 type DisabledNode = 'code' | 'link';
-type DisabledMark = 'heading' | 'blockquote' | 'code_block' | 'bullet_list' | 'ordered_list' | 'image';
+type DisabledMark =
+  | 'heading'
+  | 'blockquote'
+  | 'code_block'
+  | 'bullet_list'
+  | 'ordered_list'
+  | 'image';
 
 export interface MarkdownEditorInterface {
   id: string;
