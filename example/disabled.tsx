44d1f3cafa1e50b71ef79b128eec815315f34977
Sam McTaggart <sam.mctaggart@gmail.com>
2020-03-17 14:06:00 +0000

    Reference the correct stylesheet in the example app

--- example/disabled.tsx
+++ example/disabled.tsx
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import MarkdownEditor from '../.';
-import '../dist/markdown-editor.esm.css';
+import '../dist/markdown-editor.cjs.production.min.css';
 
 const App = () => {
   const [errors, setErrors] = React.useState('');
