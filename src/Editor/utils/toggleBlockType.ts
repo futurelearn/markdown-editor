68ad427bd12f9fb93502b0f2b2ad945a8581d4cc
Sam McTaggart <sam.mctaggart@gmail.com>
2020-04-01 11:42:43 +0100

    Handle exiting a code block correctly

--- src/Editor/utils/toggleBlockType.ts
+++ src/Editor/utils/toggleBlockType.ts
@@ -1,5 +1,5 @@
-import { setBlockType, exitCode } from 'prosemirror-commands';
-import { NodeType, Schema } from 'prosemirror-model';
+import { setBlockType } from 'prosemirror-commands';
+import { NodeType } from 'prosemirror-model';
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { nodeIsActive } from './nodeIsActive';
@@ -7,16 +7,12 @@ import { nodeIsActive } from './nodeIsActive';
 export const toggleBlockType = function(
   type: NodeType,
   toggletype: NodeType,
-  schema: Schema,
   attrs = {}
 ) {
   return (state: EditorState, dispatch: EditorView['dispatch']) => {
-    const { isActive, node } = nodeIsActive(state, type, attrs);
+    const { isActive } = nodeIsActive(state, type, attrs);
 
     if (isActive) {
-      if (type === schema.nodes.code_block && node?.textContent.length) {
-        return exitCode(state, dispatch);
-      }
       return setBlockType(toggletype)(state, dispatch);
     }
 
