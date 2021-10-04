98b2afe4258586e88e7a9475b4721a52bf320550
Seb Jacobs <me@sebjacobs.com>
2021-10-04 11:47:20 +0100

    Fix @rollup/plugin-replace warning
    After upgrading TSDX and @rollup/plugin-replace I am now seeing a
    warning when I run `npm run build`:
    
    @rollup/plugin-replace: 'preventAssignment' currently defaults to false.
    It is recommended to set this option to `true`, as the next major
    version will default this option to `true`.
    
    After some Googling the suggested solution [1] for the time being is to
    override the options passed into the plugin in our `tsdx.config.js`
    file.
    
    [1] https://github.com/jaredpalmer/tsdx/issues/981

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,9 +1,19 @@
+const replace = require('@rollup/plugin-replace');
 const postcss = require('rollup-plugin-postcss');
+
 module.exports = {
   rollup(config, options) {
-    config.plugins.push(
-      postcss({ extract: true }),
+    config.plugins = config.plugins.map(plugin =>
+      plugin.name === 'replace'
+        ? replace({
+            'process.env.NODE_ENV': JSON.stringify(options.env),
+            preventAssignment: true,
+          })
+        : plugin
     );
+
+    config.plugins.push(postcss({ extract: true }));
+
     return config;
   },
 };
